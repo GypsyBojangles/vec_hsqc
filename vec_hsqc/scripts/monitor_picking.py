90878fea8118b455c168492727be7919ba91fb8e
kieranrimmer <kieranrimmer@gmail.com>
2014-03-04 22:59:58 +1100

    more plotting added

--- vec_hsqc/scripts/monitor_picking.py
+++ vec_hsqc/scripts/monitor_picking.py
@@ -7,7 +7,9 @@ import numpy as np
 curdir = os.path.dirname( os.path.abspath( __file__ ) )
 
 datadir1 = os.path.join( curdir, 'prot1_plate1', 'plate1_reref_120319' )
-filelist1 = []
+
+
+filelist1 = [ os.path.join(datadir1,  b) for b in os.listdir(datadir1) if b[:6] == '120319' and b[:-5] != '120319_apo' and b[:11] == '120319_C2F9' ]
 
 spectrum = os.path.join( datadir1, '120319_apo.ucsf' )
 pl = os.path.join( datadir1, '120319_apo.list' )
@@ -19,13 +21,22 @@ if __name__ == '__main__':
 	a1 = vec_hsqc.read_data.ImportNmrData( msep = (2,5), threshold = 0.0 ) 
 	a1.get_data( filelist1, os.path.join(datadir1, '120319_apo.ucsf'), os.path.join(datadir1, '120319_apo.list'), 'EcDsbA', import_type = 'Training' )
 
+	print 'full_data_dict keys:', a1.full_data_dict.keys()
 	c_full = a1.full_data_dict['control']
+	c2f9_full = a1.full_data_dict['120319_C2F9.ucsf']
+
 
 	vec_hsqc.view_data.plot_2D_peaks_assigned( c_full['spectral_data'], c_full['index_list_manual'], 'control_manual', curdir )
 
 	vec_hsqc.view_data.plot_2D_peaks_assigned( c_full['spectral_data'], c_full['index_list_auto'], 'control_auto', curdir )
 
 	vec_hsqc.view_data.plot_2D_peaks_unassigned( c_full['spectral_data'], c_full['locs'], 'all_auto_peaks', curdir )
+
+	vec_hsqc.view_data.plot_2D_peaks_assigned_ppm( c_full['spectral_data'], c_full['index_list_manual'], c_full['avgheight'] * 3.0, c_full['SPobj'], 'control_manual_ppm_scale', curdir )
+	vec_hsqc.view_data.plot_2D_peaks_assigned_ppm( c_full['spectral_data'], c_full['index_list_auto'], c_full['avgheight'] * 3.0, c_full['SPobj'], 'control_auto_ppm_scale', curdir )
+	vec_hsqc.view_data.plot_2D_overlay_assigned_ppm( c_full['spectral_data'], c2f9_full['spectral_data'], c_full['index_list_auto'], c_full['avgheight'] * 3.0, c2f9_full['avgheight']*3.0, c_full['SPobj'], 'overlay_ppm_example', curdir )
+	vec_hsqc.view_data.plot_2D_overlay_zoom_ppm( c_full['spectral_data'], c2f9_full['spectral_data'], c_full['index_list_auto'], c_full['avgheight'] * 3.0, c2f9_full['avgheight']*3.0, c_full['SPobj'], 'overlay_ppm_zoom_example', curdir, X_bounds = (7,8), Y_bounds = (112,120), include_peaks = [40,164], figure_header = ' ' )
+
 
 	dic, data = ng.sparky.read( spectrum )
 
--- vec_hsqc/view_data.py
+++ vec_hsqc/view_data.py
@@ -113,4 +113,272 @@ def plot_2D_peaks_unassigned(data, picked_peaks, title, savedir):
     # save the figure
     fig.savefig( os.path.join( savedir,  title + '.png'))
 
+def plot_2D_peaks_assigned_ppm(data, picked_peaks, threshold, SPobj, title, savedir, figure_header = False):
 
+    import nmrglue as ng
+    import numpy as np
+    import matplotlib.pyplot as plt
+    import matplotlib.cm
+
+    # plot parameters
+    cmap = matplotlib.cm.Blues_r    # contour map (colors to use for contours)
+    contour_start = threshold         # contour level start value
+    contour_num = 20                # number of contour levels
+    contour_factor = 1.20           # scaling factor between contour levels
+    textsize = 6                    # text size of labels
+
+    # calculate contour levels
+    cl = contour_start * contour_factor ** np.arange(contour_num)
+
+    # read in the data from a NMRPipe file
+    #dic, data = ng.pipe.read("nmrpipe_2d/test.ft2")
+
+    # read in the integration limits
+    #peak_list = np.recfromtxt("limits.in")
+
+    peak_list = [ [ b[0], b[2] - 10, b[1] -10, b[2] + 11, b[1] + 11] for b in picked_peaks]
+    # create the figure
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+
+
+    #uc_13c = ng.pipe.make_uc(dic, data, dim=1)
+    ppm_15n = SPobj.uc0.ppm_scale()
+    ppm_15n_0, ppm_15n_1 = SPobj.uc0.ppm_limits()
+    #uc_15n = ng.pipe.make_uc(dic, data, dim=0)
+    ppm_1h = SPobj.uc1.ppm_scale()
+    ppm_1h_0, ppm_1h_1 = SPobj.uc1.ppm_limits()
+
+    # plot the contours
+    ax.contour(data, cl, cmap=cmap,
+		extent=(ppm_1h_0, ppm_1h_1, ppm_15n_0, ppm_15n_1))
+    #            extent=(0, data.shape[1] - 1, 0, data.shape[0] - 1))
+
+    # loop over the peaks
+    for name, x0, y0, x1, y1 in peak_list:
+
+        if x0 > x1:
+            x0, x1 = x1, x0
+        if y0 > y1:
+            y0, y1 = y1, y0
+
+	x0 = SPobj.uc1.ppm( x0 )
+	y0 = SPobj.uc0.ppm( y0 )
+	x1 = SPobj.uc1.ppm( x1 )
+	y1 = SPobj.uc0.ppm( y1 )
+	textpos = SPobj.uc1.ppm( x1 + 1 )
+
+        # plot a box around each peak and label
+        ax.plot([x0, x1, x1, x0, x0], [y0, y0, y1, y1, y0], 'k')
+        ax.text( x1, y0, str( int(name)), size=textsize, color='r')
+    #for peak in picked_peaks:
+    #	plt.plot( peak[2], peak[1], 'ro')
+
+    # set limits
+    #ax.set_xlim(1900, 2200)
+    #ax.set_ylim(750, 1400)
+
+    # decorate the axes
+    ax.set_ylabel("15N (ppm)")
+    ax.set_xlabel("1H (ppm)")
+    if figure_header:
+        ax.set_title( figure_header )
+    else:
+	ax.set_title( title )
+    #ax.set_xlim( ppm_1h_0 )
+    #ax.set_ylim(135, 100)
+
+
+    # Not sure why below inversions are necessary 
+    ax.invert_yaxis()
+    ax.invert_xaxis()
+
+
+
+
+    # save the figure
+    fig.savefig( os.path.join( savedir,  title + '.png'))
+
+
+
+def plot_2D_overlay_assigned_ppm(data_control, data_overlay, picked_peaks, threshold_control, threshold_overlay, SPobj, title, savedir, figure_header = False):
+
+    import nmrglue as ng
+    import numpy as np
+    import matplotlib.pyplot as plt
+    import matplotlib.cm
+
+    # plot parameters
+    cmap_control = matplotlib.cm.Blues_r    # contour map (colors to use for contours)
+    cmap_overlay = matplotlib.cm.Reds_r
+    contour_start_control = threshold_control         # contour level start value
+    contour_start_overlay = threshold_overlay         # contour level start value
+    contour_num = 20                # number of contour levels
+    contour_factor = 1.20           # scaling factor between contour levels
+    textsize = 6                    # text size of labels
+
+    # calculate contour levels
+    cl_control = contour_start_control * contour_factor ** np.arange(contour_num)
+    cl_overlay = contour_start_overlay * contour_factor ** np.arange(contour_num)
+    
+
+    # read in the data from a NMRPipe file
+    #dic, data = ng.pipe.read("nmrpipe_2d/test.ft2")
+
+    # read in the integration limits
+    #peak_list = np.recfromtxt("limits.in")
+
+    peak_list = [ [ b[0], b[2] - 10, b[1] -10, b[2] + 11, b[1] + 11] for b in picked_peaks]
+     
+    # create the figure
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+
+
+    #uc_13c = ng.pipe.make_uc(dic, data, dim=1)
+    ppm_15n = SPobj.uc0.ppm_scale()
+    ppm_15n_0, ppm_15n_1 = SPobj.uc0.ppm_limits()
+    #uc_15n = ng.pipe.make_uc(dic, data, dim=0)
+    ppm_1h = SPobj.uc1.ppm_scale()
+    ppm_1h_0, ppm_1h_1 = SPobj.uc1.ppm_limits()
+
+    # plot the contours
+    ax.contour(data_control, cl_control, cmap=cmap_control,
+		extent=(ppm_1h_0, ppm_1h_1, ppm_15n_0, ppm_15n_1))
+        
+    ax.contour(data_overlay, cl_overlay, cmap=cmap_overlay,
+		extent=(ppm_1h_0, ppm_1h_1, ppm_15n_0, ppm_15n_1))
+
+    #            extent=(0, data.shape[1] - 1, 0, data.shape[0] - 1))
+
+    # loop over the peaks
+    for name, x0, y0, x1, y1 in peak_list:
+
+        if x0 > x1:
+            x0, x1 = x1, x0
+        if y0 > y1:
+            y0, y1 = y1, y0
+
+	textpos = SPobj.uc1.ppm( x1 + 1 )
+	x0 = SPobj.uc1.ppm( x0 )
+	y0 = SPobj.uc0.ppm( y0 )
+	x1 = SPobj.uc1.ppm( x1 )
+	y1 = SPobj.uc0.ppm( y1 )
+
+        # plot a box around each peak and label
+        ax.plot([x0, x1, x1, x0, x0], [y0, y0, y1, y1, y0], 'k')
+        ax.text( textpos, y0, str( int(name)), size=textsize, color='k')
+    for peak in picked_peaks:
+    	plt.plot( SPobj.uc1.ppm(peak[2]), SPobj.uc0.ppm(peak[1]), 'gD', markersize = 3.0)
+
+    # set limits
+    #ax.set_xlim(1900, 2200)
+    #ax.set_ylim(750, 1400)
+
+    # decorate the axes
+    ax.set_ylabel("15N (ppm)")
+    ax.set_xlabel("1H (ppm)")
+    if figure_header:
+        ax.set_title( figure_header )
+    else:
+	ax.set_title( title )
+    #ax.set_xlim( ppm_1h_0 )
+    #ax.set_ylim(135, 100)
+
+
+    # Not sure why below inversions are necessary 
+    ax.invert_yaxis()
+    ax.invert_xaxis()
+
+
+
+
+    # save the figure
+    fig.savefig( os.path.join( savedir,  title + '.png'))
+
+
+
+
+def plot_2D_overlay_zoom_ppm(data_control, data_overlay, picked_peaks, threshold_control, threshold_overlay, SPobj, title, savedir, X_bounds = False, Y_bounds = False, markerstring='rD', markersize=5.0, textsize=12, include_peaks=[], figure_header = False):
+
+    import nmrglue as ng
+    import numpy as np
+    import matplotlib.pyplot as plt
+    import matplotlib.cm
+
+    # plot parameters
+    cmap_control = matplotlib.cm.Blues_r    # contour map (colors to use for contours)
+    cmap_overlay = matplotlib.cm.Greens_r
+    contour_start_control = threshold_control         # contour level start value
+    contour_start_overlay = threshold_overlay         # contour level start value
+    contour_num = 20                # number of contour levels
+    contour_factor = 1.20           # scaling factor between contour levels
+    #textsize = 6                    # text size of labels
+
+    # calculate contour levels
+    cl_control = contour_start_control * contour_factor ** np.arange(contour_num)
+    cl_overlay = contour_start_overlay * contour_factor ** np.arange(contour_num)
+    
+
+    # read in the data from a NMRPipe file
+    #dic, data = ng.pipe.read("nmrpipe_2d/test.ft2")
+
+    # read in the integration limits
+    #peak_list = np.recfromtxt("limits.in")
+
+    picked_peaks = [b for b in picked_peaks if b[0] in include_peaks]
+    peak_list = [ [ b[0], b[2] - 10, b[1] -10, b[2] + 11, b[1] + 11] for b in picked_peaks]
+     
+    # create the figure
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+
+
+    #uc_13c = ng.pipe.make_uc(dic, data, dim=1)
+    ppm_15n = SPobj.uc0.ppm_scale()
+    ppm_15n_0, ppm_15n_1 = SPobj.uc0.ppm_limits()
+    #uc_15n = ng.pipe.make_uc(dic, data, dim=0)
+    ppm_1h = SPobj.uc1.ppm_scale()
+    ppm_1h_0, ppm_1h_1 = SPobj.uc1.ppm_limits()
+
+    # plot the contours
+    ax.contour(data_control, cl_control, cmap=cmap_control,
+		extent=(ppm_1h_0, ppm_1h_1, ppm_15n_0, ppm_15n_1))
+        
+    ax.contour(data_overlay, cl_overlay, cmap=cmap_overlay,
+		extent=(ppm_1h_0, ppm_1h_1, ppm_15n_0, ppm_15n_1))
+
+    #            extent=(0, data.shape[1] - 1, 0, data.shape[0] - 1))
+
+    for peak in picked_peaks:
+        textpos_x = SPobj.uc1.ppm(peak[2] + 2)
+        textpos_y = SPobj.uc0.ppm(peak[1] + 1)
+    	plt.plot( SPobj.uc1.ppm(peak[2]), SPobj.uc0.ppm(peak[1]), markerstring, markersize = markersize)
+        ax.text( textpos_x, textpos_y, str( int(peak[0])), size=textsize, color='r', fontweight='bold')
+
+    # set limits
+    if X_bounds:
+        ax.set_xlim( X_bounds[0], X_bounds[1])
+    if Y_bounds:
+        ax.set_ylim( Y_bounds[0], Y_bounds[1])
+
+    # decorate the axes
+    ax.set_ylabel(r"$\delta \mathrm{^{15}N}$ (ppm)")
+    ax.set_xlabel(r"$\delta \mathrm{^{1}H}$ (ppm)")
+    if figure_header:
+        ax.set_title( figure_header )
+    else:
+	ax.set_title( title )
+    #ax.set_xlim( ppm_1h_0 )
+    #ax.set_ylim(135, 100)
+
+
+    # Not sure why below inversions are necessary 
+    ax.invert_yaxis()
+    ax.invert_xaxis()
+
+
+
+
+    # save the figure
+    fig.savefig( os.path.join( savedir,  title + '.png'))
